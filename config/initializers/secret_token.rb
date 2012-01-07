db21e0e8264b5e338316900b3500c02f177f0b17
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-07 22:59:57 +0800

    initial commit clean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burnbadluck::Application.config.secret_token = 'd74ac463ef6a6fcedd6d7bcc6294c04fcf346c5cd317c4ce579a5e0fc294cad9657bc219e5bd402619536b5871f22f91d5d6a91ccb5ddbe76cb91a5decc01ba9'
