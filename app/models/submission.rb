a7ad4f4c2056198401264eca04ddcf37f66a2722
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-08 00:31:53 +0800

    adding about page, implement validation

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,12 @@
 class Submission < ActiveRecord::Base
+    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :name,    
+              :presence => true
+    validates :email,   
+              :presence => true,
+              :format => { :with => email_regex }        
+    validates :content, 
+              :presence => true,
+              :length => { :maximum => 140 }
 end
