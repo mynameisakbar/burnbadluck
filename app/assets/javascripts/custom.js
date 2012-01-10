5d7de72da285455697412b4be72384e0d5b9d1f9
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-10 19:13:29 +0800

    initial implementation of facebook sharing

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -55,7 +55,7 @@ $(function(){
     });
 });
 
-// this is for FACEBOOK
+// this is for FACEBOOK LIKE
 (function(d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) return;
@@ -66,4 +66,31 @@ $(function(){
 
 // this is for TWITTER
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+// this is for GANALYTICS
+var _gaq = _gaq || [];
+  	_gaq.push(['_setAccount', 'UA-28207206-1']);
+  	_gaq.push(['_trackPageview']);
+ 
+(function() {
+    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// this is for FB SHARE
+$(function(){
+    $('#share_button').click(function(e){
+        e.preventDefault();
+        FB.ui(
+        {
+            method: 'feed',
+            name: 'This is the content of the "name" field.',
+            link: ' http://burnbadluck.com/',
+            caption: 'This is the content of the "caption" field.',
+            description: 'This is the content of the "description" field, below the caption.',
+            message: ''
+        });
+    });
+});
         
