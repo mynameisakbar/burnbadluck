d3433596fb45edfb4ec521a8479a8bc0daace8fd
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-12 04:24:03 +0800

    rolling back the email sender code for staging

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,12 +1,12 @@
 class Notifier < ActionMailer::Base
-    default :from => "info@burnbadluck.com"
+    #default :from => "info@burnbadluck.com"
     
     # send a signup email to the user, pass in the user object that contains the user's email address
-    def signup_email(user)
-        mail
-        ( 
-             :to => user.email, 
-             :subject => "Thanks for burning with us" 
-        )
-    end
+    #def signup_email(user)
+    #mail
+    #    ( 
+    #        :to => user.email, 
+    #         :subject => "Thanks for burning with us" 
+    #    )
+    #end
 end
