9a7ed6c8353a1ca5866b98adca40eec0d9dd54d3
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-11 14:25:20 +0800

    adjust for google analytics ;

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -75,17 +75,6 @@ $(function(){
 // this is for TWITTER
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-// this is for GANALYTICS
-var _gaq = _gaq || [];
-  	_gaq.push(['_setAccount', 'UA-28207206-1']);
-  	_gaq.push(['_trackPageview']);
- 
-(function() {
-    	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 // this is for FB SHARE
 $(function(){
     var id = $("#share_button_").attr("alt");
