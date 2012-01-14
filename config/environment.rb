e8ea0a636fd331d027cb4b241be40ba6d4239a45
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-14 08:43:15 +0800

    mail fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Burnbadluck::Application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
     :user_name => "app2379554@heroku.com",
     :password => "g5fpcxgh",
