8ca023d30fd6df0aa5b61506af7bc8bf4744666f
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-16 06:39:34 +0800

    for akbar

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,7 +2,6 @@ class SubmissionsController < ApplicationController
   USERS = { "admin" => "burnbabyburn" }
   before_filter :auth, :only => :index
     
-  http_basic_authenticate_with :name => "admin", :password => "moderator", :only => :destroy 
   http_basic_authenticate_with :name => "admin", :password => "moderator", :only => :update
 
   # GET /submissions/1
