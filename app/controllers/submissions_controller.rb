a26cf90b897f429ec03bbff46e4ce28e3c3c852c
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-15 17:58:27 +0800

    sabar nyet

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -80,7 +80,7 @@ class SubmissionsController < ApplicationController
   # GET /submissions.json
   def index
       @submissions = Submission.paginate(:page => params[:page], 
-                                         :per_page => 2,
+                                         :per_page => 5,
                                          :order => "created_at DESC")
       #@submissions = Submission.all
       @submission = Submission.new
