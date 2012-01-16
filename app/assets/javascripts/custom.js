7a5562bdf423585d2f25721cc2d3cf4469df3a43
Prima Aulia Gusta <primAulia@gmail.com>
2012-01-16 17:43:47 +0800

    last push from boy

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -64,7 +64,7 @@ $(function(){
     
     /* this is for COUNTDOWN TIMER*/
     $('#cny_countdown').countdown({
-        until: $.countdown.UTCDate(+8, 2012, 1 - 1, 21, 9)});
+        until: $.countdown.UTCDate(+8, 2012, 1 - 1, 21, 21)});
     
     // this is for validation 
     //$("#new_submission").validate();
